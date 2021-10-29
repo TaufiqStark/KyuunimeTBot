0e7607195f14fe7f10fdd477f5b61198ae65029a
Ｂｌｕｉｌｄ <48915546+TaufiqStark@users.noreply.github.com>
2021-10-29 15:12:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const fs = require('fs');
 const bot = new Telegraf(TOKEN);
 const { GetLink, DLFunc } = require('./zippy.js');
 const { neoUpdate, neoLink, neOngoing } = require('./neonime.js');
+function randText(){
+  char = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  rText = ''
+  for(i=1; i<=20; i++){
+    rIn = Math.floor(Math.random() * char.length)
+    rChar = rIn%2 ? char[rIn] : typeof(char[rIn]) == 'number' ? char[rIn] : char[rIn].toUpperCase()
+    rText+=rChar.toString()
+  }
+  return rText;
+}     
 
 function makeButton(blu, msg, ...btn){
   //console.log(btn)
