3bc140ba00b548af29461f9e6a2ac354f33f044b
Ｂｌｕｉｌｄ <48915546+TaufiqStark@users.noreply.github.com>
2021-12-06 21:29:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ bot.on('video', async (ctx) => {
 prm = 'v'+randText()
 dbtg = await axios('https://db.kyuki.tk/tg').then(res=>res.data)
 dbtg.video[prm] = ctx.message
-axios.post('https://db.kyuki.tk/tg', JSON.stringify(dbtg, null, 2))
+await axios.post('https://db.kyuki.tk/tg', JSON.stringify(dbtg, null, 2))
 await ctx.reply(`https://t.me/${ctx.botInfo.username}?start=${prm}`,{reply_to_message_id: ctx.message.message_id})
 })
 bot.on('message', async (blu, next) => {
